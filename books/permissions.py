3ca8361faf6ba9b7cac54225fd6d54fc7af7f3b2
amiaya <amiayaprime@gmail.com>
2023-04-09 13:30:24 +0100

    feat: create a custom permission

---
+++ books/permissions.py
@@ -0,0 +1,15 @@
+from rest_framework import permissions
+
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow admins to create or update books.
+    """
+
+    def has_permission(self, request, view):
+        if request.method in permissions.SAFE_METHODS:
+            # Allow read-only access for all users
+            return True
+
+        # Only allow admins to create or update books
+        return request.user and request.user.is_authenticated and request.user.role == 'admin'
