c133930f94f9115b986d16a45cc6fe395d216915
chidi-godwin <chidieberen1998@gmail.com>
2023-04-12 01:03:41 +0100

    add filter to book list create view

--- books/urls.py
+++ books/urls.py
@@ -1,17 +1,13 @@
 from django.urls import path
-from .views import AuthorDetailView, AuthorListCreateView, BookByAuthorListView, BookByCategoryListView, BookDetailView, BookListCreateView, CategoryDetailView, CategoryListCreateView
+from .views import BookDetailView, BookListCreateView, CategoryDetailView, CategoryListCreateView
+from rest_framework.urlpatterns import format_suffix_patterns
 
+app_name = 'books'
 
-urlpatterns = [
-    path('authors/', AuthorListCreateView.as_view(), name='author-list'),
-    path('authors/<int:pk>/', AuthorDetailView.as_view(), name='author-detail'),
-    path('books/', BookListCreateView.as_view(), name='book-list'),
-    path('books/<int:pk>/', BookDetailView.as_view(), name='book-detail'),
+urlpatterns = format_suffix_patterns([
+    path('', BookListCreateView.as_view(), name='book-list'),
+    path('<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('categories/', CategoryListCreateView.as_view(), name='category-list'),
     path('categories/<int:pk>/', CategoryDetailView.as_view(),
          name='category-detail'),
-    path('authors/<int:author_id>/books/',
-         BookByAuthorListView.as_view(), name='books-by-author'),
-    path('categories/<int:category_id>/books/',
-         BookByCategoryListView.as_view(), name='books-by-category'),
-]
+])
--- books/views.py
+++ books/views.py
@@ -1,35 +1,16 @@
-from rest_framework import generics, status
-
+from rest_framework import generics
 from books.permissions import IsAdminOrReadOnly
-from .models import Author, Book, Category
-from .serializers import AuthorSerializer, BookSerializer, CategorySerializer
-from rest_framework.response import Response
+from .models import Book, Category
+from .serializers import BookSerializer, CategorySerializer
 from rest_framework.permissions import IsAuthenticated
-
-
-class AuthorListCreateView(generics.ListCreateAPIView):
-    queryset = Author.objects.all()
-    serializer_class = AuthorSerializer
-    permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
-
-
-class AuthorDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Author.objects.all()
-    serializer_class = AuthorSerializer
-    permission_classes = [IsAuthenticated]
+from .filters import BookFilter
 
 
 class BookListCreateView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     permission_classes = [IsAuthenticated, IsAdminOrReadOnly]
-
-    def get_queryset(self):
-        queryset = super().get_queryset()
-        title = self.request.query_params.get('title', None)
-        if title is not None:
-            queryset = queryset.filter(title__icontains=title)
-        return queryset
+    filterset_class = BookFilter
 
 
 class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
@@ -48,21 +29,3 @@ class CategoryDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
     permission_classes = [IsAuthenticated]
-
-
-class BookByAuthorListView(generics.ListAPIView):
-    serializer_class = BookSerializer
-    # permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        author_id = self.kwargs.get('author')
-        return Book.objects.filter(authors__id=author_id)
-
-
-class BookByCategoryListView(generics.ListAPIView):
-    serializer_class = BookSerializer
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        category_id = self.kwargs.get('category')
-        return Book.objects.filter(category__id=category_id)
