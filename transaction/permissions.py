c1cdb83310a43724415bd91bc1cfd2d552582dee
chidi-godwin <chidieberen1998@gmail.com>
2023-04-12 16:52:41 +0100

    implement permissions for cart and cartItem owner

---
+++ transaction/permissions.py
@@ -0,0 +1,10 @@
+from rest_framework.permissions import BasePermission
+
+
+class IsCartOwner(BasePermission):
+  def has_object_permission(self, request, view, obj):
+    return obj.user == request.user
+  
+class IsCartItemOwner(BasePermission):
+  def has_object_permission(self, request, view, obj):
+    return obj.cart.user == request.user
