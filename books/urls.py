001ce6fcf385b4b548a1f0c215f1fcc07bf25eab
chidi-godwin <chidieberen1998@gmail.com>
2023-04-12 01:16:02 +0100

    create index view for api root view

--- books/urls.py
+++ books/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import BookDetailView, BookListCreateView, CategoryDetailView, CategoryListCreateView
+from .views import BookDetailView, BookListCreateView, CategoryDetailView, CategoryListCreateView, index
 from rest_framework.urlpatterns import format_suffix_patterns
 
 app_name = 'books'
 
 urlpatterns = format_suffix_patterns([
     path('', BookListCreateView.as_view(), name='book-list'),
+    path('index/', index, name='index'),
     path('<int:pk>/', BookDetailView.as_view(), name='book-detail'),
     path('categories/', CategoryListCreateView.as_view(), name='category-list'),
     path('categories/<int:pk>/', CategoryDetailView.as_view(),
--- books/views.py
+++ books/views.py
@@ -1,10 +1,22 @@
 from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 from books.permissions import IsAdminOrReadOnly
 from .models import Book, Category
 from .serializers import BookSerializer, CategorySerializer
 from rest_framework.permissions import IsAuthenticated
 from .filters import BookFilter
 
+
+@api_view(['GET'])
+def index(request, format=None):
+  return Response({
+    'list or create books': reverse('books:book-list', request=request, format=format),
+    'update, retrieve or delete book detials': reverse('books:book-detail', request=request, format=format),
+    'list or create categories': reverse('books:category-list', request=request, format=format),
+    'update, retrieve or delete category detials': reverse('books:category_detail', request=request, format=format)
+  })
 
 class BookListCreateView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
