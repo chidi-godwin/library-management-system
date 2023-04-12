4337619958d4ab3f8ac0cef5145ebf2e630aacb7
chidi-godwin <chidieberen1998@gmail.com>
2023-04-12 17:20:59 +0100

    make comments field nullable

--- transaction/models.py
+++ transaction/models.py
@@ -29,7 +29,7 @@ class Transaction(models.Model):
   )
   
   status = models.CharField(max_length=10, choices=STATUS, default='pending')
-  comments = models.TextField()
+  comments = models.TextField(blank=True, null=True)
   total = models.DecimalField(max_digits=15, decimal_places=2)
   user = models.ForeignKey('users.User', on_delete=models.CASCADE, related_name='transactions')
   created_at = models.DateTimeField(auto_now_add=True)
